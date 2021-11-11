7b11d77b34e5cb6a7e7c40a38bb4a3e1a39750a8
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2021-11-11 15:25:30 +0700

    Add files via upload

---
+++ pages/users.js
@@ -0,0 +1,105 @@
+import React, {useEffect, useState } from "react";
+import styles from "../styles/users.module.scss";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faPen,
+  faWindowClose,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import Modal from "react-modal";
+import AdminDetail from "react-modal";
+import EditAdmin from "react-modal";
+import Swal from "sweetalert2";
+
+(Modal, AdminDetail, EditAdmin).setAppElement();
+
+
+const Users = () => {
+
+    return (
+      <section className={styles.article}>
+        <h1 style={{ lineHeight: "10px", fontFamily: "comfortaa"}}> User </h1>
+
+        <div className={styles.header}>
+        <div className={styles.search}>
+          <form>
+            {" "}
+            <input name="Search"
+                   type="text"
+                   placeholder="Search User..."
+                  //  onChange={}
+                  // value={}
+                   className="input-search"
+            />
+          </form>
+        </div>
+      </div>
+
+      <section>
+      <section className={styles.users}>
+        <title>User</title>
+
+        <div className={styles.tblUsers}>
+          <div className={styles.column1}>
+            <span>Email</span>
+          </div>
+
+          <div className={styles.column2}>
+            <span>Username</span>
+          </div>
+
+          <div className={styles.column3}>
+            <div>
+              <span>Phone</span>
+            </div>
+          </div>
+
+          <div className={styles.column4}>
+            <div>
+              <span>Action</span>
+            </div>
+            </div>
+          </div>
+                <div className={styles.column}>
+                      {/* DETAIL PRODUCT */}
+                      <button
+                        className={styles.btnAction}
+                      >
+                        <FontAwesomeIcon
+                          icon={faInfoCircle}
+                          size="1x"
+                          style={{ color: "black" }}
+                        />
+                      </button>
+
+                      {/* EDIT PRODUCT */}
+                      <button
+                        className={styles.btnAction}
+                      >
+                        <FontAwesomeIcon
+                          icon={faPen}
+                          size="1x"
+                          style={{ color: "black" }}
+                        />
+                      </button>
+
+                      {/* DELETE PRODUCT */}
+                      <button
+                        className={styles.btnAction}
+                      >
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          size="1x"
+                          style={{ color: "black" }}
+                        />
+                      </button>
+                    </div>
+      </section>
+    </section>
+  </section>
+);
+};
+
+export default Users;
